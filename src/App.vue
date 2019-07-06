a2ce536552c5682c8da06e9f27cf4882894ed2b8
taraschuiko <taraschuiko@gmail.com>
2019-07-06 12:52:47 +0300

    Made Home component with home background

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -23,6 +22,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+  max-width: 960px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img src="../assets/smart-home.svg" alt="Smart home" class="home__background">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: auto;
+  
+  &__background {
+    width: 100%;
+  }
+}
+</style>
